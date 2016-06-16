49b911cfb9ca91aca520112385f5d131f9b3ffea
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-06-16 11:29:19 -0500

    psm: redesign (returns delta now, not gamma)

--- R/psm.R
+++ R/psm.R
@@ -1,17 +1,18 @@
 psm <- function(mcfile="mcmc.RData") {
   load(mcfile)
   X <- as.matrix(jagsfit$mcmc)
-  pmx <- apply(X, 2, median)
-  psx <- apply(X, 2, mad)
-  v <- varnames(jagsfit$mcmc)
-  i <- as.numeric(gsub(",.+", "", gsub(".+\\[", "", v)))
-  j <- as.numeric(gsub("\\]", "", gsub(".+,", "", v)))
-  pm <- ps <- matrix(NA, max(i), max(j))
-  for (m in 1:ncol(X)) {
-    pm[i[m],j[m]] <- pmx[m]
-    ps[i[m],j[m]] <- psx[m]
+  if (Data$arrayBait) {
+    dim(X) <- c(nrow(X), dim(Data$Bait)[1], dim(Data$Bait)[3])
+    pm <- cbind(apply(X, 2:3, median), apply(X[,,2]-X[,,1], 2, median))
+    ps <- cbind(apply(X, 2:3, mad), apply(X[,,2]-X[,,1], 2, mad))
+    b <- dimnames(Data$Bait)[[3]]
+    colnames(pm) <- colnames(ps) <- c(b, paste(b[2], b[1], sep="_"))
+    rownames(pm) <- rownames(ps) <- dimnames(Data$Bait)[[1]]
+  } else {
+    pm <- apply(X, 2, median)
+    ps <- apply(X, 2, mad)
+    names(pm) <- names(ps) <- dimnames(Data$Bait)[[1]]
   }
-  dimnames(pm) <- dimnames(ps) <- dimnames(Data$Bait)[c(1,3)]
   structure(list(Data=Data, pm=pm, ps=ps),
             class = "psm.deepn")
 }
