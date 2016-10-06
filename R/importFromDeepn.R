50d666f62c9ec96ecc800c4f663a44ff371754f8
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-10-06 10:49:22 -0500

    Bug fix: If config file from DEEPN is out of order

--- R/importFromDeepn.R
+++ R/importFromDeepn.R
@@ -3,6 +3,9 @@ importFromDeepn <- function(file) {
   config <- matrix(trimws(unlist(strsplit(raw, "="))), ncol=2, byrow=TRUE)
   key <- config[,1]
   val <- config[,2]
+  ind <- order(key)
+  key <- key[ind]
+  val <- val[ind]
   vn <- val[grep("Vector_Non", key)]
   vs <- val[grep("Vector_Sel", key)]
   bn <- val[grep("Bait._Non", key)]
