709213efab29a54980d38f6d026e688c5299c089
Patrick Breheny <patrick-breheny@uiowa.edu>
2016-10-12 14:57:21 -0500

    Checks for bait effects

--- R/mcmc.R
+++ R/mcmc.R
@@ -5,11 +5,11 @@ runMCMC <- function(Data, n.chains=4) {
   monitor <- c('gamma')
   if (Data$multiBait) {
     nb <- dim(Data$Bait)[3]
-    jData <- with(Data, list(v=Vector, y=Bait, ng=ng, nb=nb, nvr=nvr, ytr=btr, vtr=vtr, omega=omega))
+    jData <- with(Data, list(v=Vector, y=Bait, ng=ng, nb=nb, nvr=nvr, ytr=btr, vtr=vtr, omega=omega[3:2]))
     model <- system.file("model/sModel2.jag", package="deepn")
     nsig <- 2 + nb
   } else {
-    jData <- with(Data, list(v=Vector, y=Bait, ng=ng, nvr=nvr, ytr=btr, vtr=vtr, omega=omega))
+    jData <- with(Data, list(v=Vector, y=Bait, ng=ng, nvr=nvr, ytr=btr, vtr=vtr, omega=omega[3:2]))
     model <- system.file("model/sModel1.jag", package="deepn")
     nsig <- 3
   }
--- R/overdisp.R
+++ R/overdisp.R
@@ -3,5 +3,12 @@ overdisp <- function(Data) {
   omega <- c(edgeR::estimateCommonDisp(edgeR::DGEList(V[,1,]))$common.dispersion,
              edgeR::estimateCommonDisp(edgeR::DGEList(V[,2,]))$common.dispersion)
   names(omega) <- dimnames(V)[[2]]
+  if (!is.null(Data$Bait)) {
+    B <- if(Data$multiBait) Data$Bait[,1,] else Data$Bait[,1,drop=FALSE]
+    S <- cbind(V[,1,], B)
+    colnames(S) <- NULL
+    omega[3] <- edgeR::estimateCommonDisp(edgeR::DGEList(S))$common.dispersion
+    names(omega)[3] <- "baitEffect"
+  }
   omega
 }
